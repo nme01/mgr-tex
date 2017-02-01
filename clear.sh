dd10805f71c9aeccc7d5fed849429e8f98d397a3
Jacek Witkowski <jacek.witkowski@cern.ch>
2017-02-01 21:36:29 +0100

    Added initial version of thesis

---
+++ clear.sh
@@ -0,0 +1,12 @@
+﻿#!/bin/bash
+rm tex/*.aux
+rm tex/*.bbl
+rm tex/*.bcf
+rm tex/*.blg
+rm tex/*.log
+rm tex/*.out
+rm tex/*.pdf
+rm tex/*.run.xml
+rm tex/*.synctex.gz
+rm tex/*.tex.*
+rm tex/*.toc
---
+++ generate.sh
@@ -0,0 +1,5 @@
+﻿#!/bin/bash
+pdflatex tex/main.tex
+bibtex "main"
+pdflatex tex/main.tex
+pdflatex tex/main.tex
