c808336ed6cdc3fba02d80084e467904e5cc73b7
jwitkows <jacek.witkowski@gmail.com>
2017-03-16 14:56:45 +0100

    Skrypt open_pdf.py dziala zarowno na Windowsie jak i Linuxie.

--- open_pdf.py
+++ open_pdf.py
@@ -1,6 +1,21 @@
 import os
+from sys import platform
+
 from constants import OUT_DIRECTORY
 
 
+def open_pdf():
+    if platform == "linux" or platform == "linux2":
+        shell_cmd = "see"
+    elif platform == "win32":
+        shell_cmd = "start"
+    else:
+        raise ValueError("Unknown platform type: " + platform)
+
+    path_to_pdf_file = os.path.join(OUT_DIRECTORY, "main.pdf")
+    full_cmd = "".join([shell_cmd, " ", path_to_pdf_file])
+    os.system(full_cmd)
+
+
 if __name__ == "__main__":
-    os.system("start " + os.path.join(OUT_DIRECTORY, "main.pdf"))
+    open_pdf()
